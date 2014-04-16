f37387d1576b5076420f2e93fe0810199f030b5e
lswroc01 <laura.wrocklage@gmail.com>
2014-04-16 00:31:38 -0400

    Initial Commit

---
+++ ProductStore/ProductStore/Models/OrdersContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace ProductStore.Models
+{
+    public class OrdersContext : DbContext
+    {
+        public OrdersContext()
+            : base("name=OrdersContext")
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
